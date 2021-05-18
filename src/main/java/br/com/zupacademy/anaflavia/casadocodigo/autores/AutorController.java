3f9bf6016d997fb52fa68ae88439e3cb6953825b
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2021-05-18 17:06:05 -0300

    email-unico

--- src/main/java/br/com/zupacademy/anaflavia/casadocodigo/autores/AutorController.java
+++ src/main/java/br/com/zupacademy/anaflavia/casadocodigo/autores/AutorController.java
@@ -1,19 +1,34 @@
 package br.com.zupacademy.anaflavia.casadocodigo.autores;
 
+
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zupacademy.anaflavia.casadocodigo.autores.dto.AutorDto;
+import br.com.zupacademy.anaflavia.casadocodigo.validacoes.RestricaoEmailDuplicadoValidator;
 
 @RestController
 @RequestMapping("/autor")
 public class AutorController{
+	
+	@Autowired
+	private RestricaoEmailDuplicadoValidator restricaoEmailDuplicadoValidator;
+	
+	@InitBinder
+	public void init(WebDataBinder binder) {
+		
+		binder.addValidators(restricaoEmailDuplicadoValidator);
+		
+	}
 	
 	@Autowired
 	private AutorRepository autorRepository;
---
+++ src/main/java/br/com/zupacademy/anaflavia/casadocodigo/validacoes/RestricaoEmailDuplicadoValidator.java
@@ -0,0 +1,43 @@
+package br.com.zupacademy.anaflavia.casadocodigo.validacoes;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import br.com.zupacademy.anaflavia.casadocodigo.autores.Autor;
+import br.com.zupacademy.anaflavia.casadocodigo.autores.AutorRepository;
+import br.com.zupacademy.anaflavia.casadocodigo.autores.dto.AutorDto;
+
+@Component
+public class RestricaoEmailDuplicadoValidator implements Validator {
+	
+	@Autowired
+	private AutorRepository autorRepository;
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+	   return AutorDto.class.isAssignableFrom(clazz);	
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		if(errors.hasErrors()) {
+			return;	
+		}
+		
+		AutorDto autorDto = (AutorDto) target;
+		 Optional<Autor> candidatoAutor = autorRepository.findByEmail(autorDto.getEmail());
+	       
+		 if(candidatoAutor.isPresent()) {
+			 errors.rejectValue("email", null,
+					 "Ja existe cadastro com este email "  + autorDto.getEmail());
+		 }
+	}
+	
+	
+	
+}
+
